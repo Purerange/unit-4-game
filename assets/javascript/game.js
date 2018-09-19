492eeb9535c1826b2d79bfb48c9960b8b1d523c8
Stephen Marsh <38229184+Purerange@users.noreply.github.com>
2018-09-19 13:40:36 -0700

    created game page and started thinking through game logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var currentScore;
+var randomNumber;
+var wins;
+var losses;
+var crystal1Value;
+var crystal2Value;
+var crystal3Value;
+var crystal4Value;
+
+$(function(){
+    alert("test");
+});
